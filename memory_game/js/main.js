bb3c27144d4fe4d5b90182285f9db0131367cdd1
CoreyJNash <CoreyJNash@gmail.com>
2018-06-01 16:09:08 -0400

    Made groups for cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,43 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+ {
+   rank: "Queen",
+   suit: "Diamonds",
+   cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+   rank: "King",
+   suit: "Hearts",
+   cardImage: "images/king-of-hearts.png"
+ },
+ {
+   rank: "King",
+   suit: "Diamonds",
+   cardImage: "images/king-of-diamonds.png"
+ },
+];
 
 var cardsInPlay = [];
 var checkForMatch = function(){
-if (cardsInPlay[0] === cardsInPlay[1]){
+if (cardsInPlay[0] === cardsInPlay[2]){
  alert ("You Found a Match!");
 } else {
  alert ("Sorry, Try Again!");
 }
 }
-var flipCard = function(cardID){
-console.log("User flipped " + cards[cardID])
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId].rank)
+console.log("User flipped " + cards[cardId].suit)
+console.log("User flipped " + cards[cardId].cardImage)
 
-cardsInPlay.push(cards[cardID]);
+cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2){
 checkForMatch();
 }
 };
 flipCard(0);
-flipCard(1);
+flipCard(2);
