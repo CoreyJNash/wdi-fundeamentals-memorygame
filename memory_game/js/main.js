487ce1e223f4ad86af207239a5ca62ba06816f41
CoreyJNash <CoreyJNash@gmail.com>
2018-06-01 13:35:18 -0400

    Created organized game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped Queen")
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log('User flipped King')
-
-if (cardsInPlay.length === 2){
- if (cardsInPlay[0] === cardsInPlay[1]){
-  alert ("You Found a Match!");
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]){
+ alert ("You Found a Match!");
 } else {
-  alert ("Sorry, Try Again!");
+ alert ("Sorry, Try Again!");
 }
 }
+var flipCard = function(cardID){
+console.log("User flipped " + cards[cardID])
+
+cardsInPlay.push(cards[cardID]);
+if (cardsInPlay.length === 2){
+checkForMatch();
+}
+};
+flipCard(0);
+flipCard(1);
