098faf695853ecf74ea2e4349567c4a5b2d4820c
CoreyJNash <CoreyJNash@gmail.com>
2018-06-02 23:15:31 -0400

    Finished and completed the memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,26 +18,36 @@ var cards = [
    rank: "King",
    suit: "Diamonds",
    cardImage: "images/king-of-diamonds.png"
- },
+ }
 ];
 
 var cardsInPlay = [];
-var checkForMatch = function(){
-if (cardsInPlay[0] === cardsInPlay[2]){
+
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[1]){
  alert ("You Found a Match!");
 } else {
  alert ("Sorry, Try Again!");
 }
-}
-var flipCard = function(cardId){
-console.log("User flipped " + cards[cardId].rank)
-console.log("User flipped " + cards[cardId].suit)
-console.log("User flipped " + cards[cardId].cardImage)
+};
 
+var flipCard = function() {
+cardId = this.getAttribute('data-id');
+console.log(cardId);
 cardsInPlay.push(cards[cardId].rank);
-if (cardsInPlay.length === 2){
-checkForMatch();
+this.setAttribute('src', cards[cardId].cardImage);
+if (cardsInPlay.length === 2) {
+  checkForMatch();
 }
 };
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+  createBoard();
